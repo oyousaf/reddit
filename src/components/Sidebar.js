56b0c2d6a514525374aa3ea025c05de708593a15
Omar ♔ <o_yousaf@live.co.uk>
2023-12-22 18:52:08 +0000

    squashing bugs

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,27 +2,27 @@ import React from "react";
 
 const Sidebar = () => {
   const popularSubreddits = [
-    "reactjs",
-    "programming",
-    "javascript",
-    "redux",
-    "tailwind",
+    "Reactjs",
+    "Programming",
+    "Javascript",
+    "Redux",
+    "Tailwind",
   ];
   return (
     <>
-      <h2 className="text-white text-xl font-semibold mb-4 uppercase">
-        Popular
-      </h2>
-      <ul className="space-y-2">
-        {popularSubreddits.map((subreddit) => (
-          <li
-            key={subreddit}
-            className="text-white hover:text-gray-300 cursor-pointer"
-          >
-            {subreddit}
-          </li>
-        ))}
-      </ul>
+    <h2 className="text-white text-xl font-semibold mb-4 uppercase">
+    Popular
+    </h2>
+    <ul className="space-y-2">
+      {popularSubreddits.map((subreddit) => (
+        <li
+          key={subreddit}
+          className="text-white hover:text-gray-300 cursor-pointer"
+        >
+          {subreddit}
+        </li>
+      ))}
+    </ul>
     </>
   );
 };
