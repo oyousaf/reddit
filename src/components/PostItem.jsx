a5f2955687ac028afa170685b0b1bb33f50e5576
Omar ♔ <o_yousaf@live.co.uk>
2023-12-22 17:24:54 +0000

    squashing bugs

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -2,11 +2,26 @@ import React from "react";
 import { FaArrowUp, FaArrowDown, FaRegCommentAlt } from "react-icons/fa";
 
 const PostItem = ({ post, onItemClick }) => {
-  const hasMedia = post.media || post.url_overridden_by_dest || post.thumbnail;
-  const isVideoPost = post.is_video;
-  const isExternalLink = post.post_hint === "link";
+  const isImagePost = post.post_hint === "image";
+  const isVideoPost = post.post_hint === "hosted:video";
 
-  console.log(post);
+  const calculateTimeDifference = (utcTimestamp) => {
+    const currentUtcTimestamp = Math.floor(Date.now() / 1000);
+    const secondsDifference = currentUtcTimestamp - utcTimestamp;
+
+    if (secondsDifference < 60) {
+      return `${secondsDifference} seconds ago`;
+    } else if (secondsDifference < 3600) {
+      const minutes = Math.floor(secondsDifference / 60);
+      return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+    } else if (secondsDifference < 86400) {
+      const hours = Math.floor(secondsDifference / 3600);
+      return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+    } else {
+      const formattedDate = new Date(utcTimestamp * 1000).toLocaleString();
+      return `Posted on ${formattedDate}`;
+    }
+  };
 
   return (
     <li className="cursor-pointer border-b border-teal-700 p-4 hover:bg-teal-800 transition duration-300 rounded-md sm:flex">
@@ -23,39 +38,26 @@ const PostItem = ({ post, onItemClick }) => {
             <FaRegCommentAlt /> {post.num_comments} Comments
           </span>
         </div>
-        {hasMedia ? (
+        {isImagePost ? (
+          <p className="mt-2">{post.selftext.substring(0, 100)}...</p>
+        ) : (
           <div className="flex items-center mt-2 text-gray-300">
             <span>Posted by u/{post.author}</span>
             <span className="mx-2">•</span>
-            <span>{new Date(post.created_utc * 1000).toLocaleString()}</span>
+            <span>{calculateTimeDifference(post.created_utc)}</span>
           </div>
-        ) : null}
+        )}
       </div>
-      {hasMedia && (
-        <>
-          {isVideoPost ? (
-            <div className="mt-2 rounded sm:w-1/3 overflow-hidden">
-              <video controls width="100%">
-                <source
-                  src={post.media.reddit_video?.fallback_url || post.url}
-                  type="video/mp4"
-                />
-                Your browser does not support the video tag.
-              </video>
-            </div>
-          ) : isExternalLink ? (
-            <a href={post.url} target="_blank" rel="noopener noreferrer">
-              <img src={post.thumbnail} className="mt-2 rounded w-[100px]" />
-            </a>
-          ) : (
-            <img
-              src={post.url_overridden_by_dest || post.thumbnail}
-              alt="Post Thumbnail"
-              className="mt-2 rounded w-[300px]"
-            />
-          )}
-        </>
-      )}
+      {isImagePost ? (
+        <img
+          src={post.url_overridden_by_dest || post.thumbnail}
+          alt="Post Thumbnail"
+          className="mt-2 rounded sm:w-1/3"
+        />
+      ) : isVideoPost ? (
+        // You can render a video player or any specific UI for videos
+        <p>Video Post</p>
+      ) : null}
     </li>
   );
 };
