828cae4827d524c2cbd4803e1803f972e0be5449
Omar ♔ <o_yousaf@live.co.uk>
2023-12-23 15:51:19 +0000

    squashing bugs

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -13,21 +13,22 @@ const Sidebar = () => {
     "science",
     "pics",
   ];
+
   return (
     <>
-    <h2 className="text-white text-xl font-semibold mb-4 uppercase">
-    Popular
-    </h2>
-    <ul className="space-y-2">
-      {popularSubreddits.map((subreddit) => (
-        <li
-          key={subreddit}
-          className="text-white hover:text-gray-300 cursor-pointer"
-        >
-          r/{subreddit}
-        </li>
-      ))}
-    </ul>
+      <h2 className="text-white text-xl font-semibold mb-4 uppercase">
+        Popular
+      </h2>
+      <ul className="space-y-2">
+        {popularSubreddits.map((subreddit) => (
+          <li
+            key={subreddit}
+            className="text-white hover:text-gray-300 cursor-pointer"
+          >
+            r/{subreddit}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
