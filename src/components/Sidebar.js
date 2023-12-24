345bfded7e7fda5d8b99edf6f9cbc6bedbb31b32
Omar ♔ <o_yousaf@live.co.uk>
2023-12-24 15:37:49 +0000

    added toggle for sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { FiChevronDown, FiChevronUp } from "react-icons/fi";
 
 const Sidebar = () => {
   const popularSubreddits = [
@@ -14,22 +15,42 @@ const Sidebar = () => {
     "pics",
   ];
 
+  const [isListVisible, setListVisibility] = useState(false);
+
+  const toggleListVisibility = () => {
+    setListVisibility(!isListVisible);
+  };
+
   return (
-    <>
-      <h2 className="text-white text-xl font-semibold mb-4 uppercase">
-        Popular
-      </h2>
-      <ul className="space-y-2">
-        {popularSubreddits.map((subreddit) => (
-          <li
-            key={subreddit}
-            className="text-white hover:text-gray-300 cursor-pointer"
-          >
-            r/{subreddit}
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className="flex flex-col text-center items-center">
+      <div className="flex">
+        <h2 className="text-white text-xl font-semibold mb-4 uppercase">
+          Popular
+        </h2>
+        <button
+          className="md:hidden text-white focus:outline-none ml-2"
+          onClick={toggleListVisibility}
+        >
+          {isListVisible ? <FiChevronUp /> : <FiChevronDown />}
+        </button>
+      </div>
+      <div className="md:flex">
+        <ul
+          className={`space-y-2 md:flex flex-col ${
+            isListVisible ? "block" : "hidden"
+          }`}
+        >
+          {popularSubreddits.map((subreddit) => (
+            <li
+              key={subreddit}
+              className="text-white hover:text-gray-300 cursor-pointer"
+            >
+              r/{subreddit}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 };
 
