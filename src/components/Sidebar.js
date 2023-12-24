85342308f317e45a897dd6007f769d018347bf81
Omar ♔ <o_yousaf@live.co.uk>
2023-12-24 17:59:51 +0000

    squashing bugs

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -22,28 +22,22 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="flex flex-col text-center items-center">
-      <div className="flex">
-        <h2 className="text-white text-xl font-semibold mb-4 uppercase">
-          Popular
-        </h2>
+    <div className="flex flex-col text-center">
+      <div className="flex justify-center items-center mb-4">
+        <h2 className="text-white text-xl font-semibold uppercase">Popular</h2>
         <button
           className="md:hidden text-white focus:outline-none ml-2 text-2xl"
           style={{
             transition: "transform 0.3s ease-in-out",
-            transform: isListVisible ? "rotate(180deg)" : "rotate(0deg)",
+            transform: `rotate(${isListVisible ? 180 : 0}deg)`,
           }}
           onClick={toggleListVisibility}
         >
-          {isListVisible ? <FiChevronUp /> : <FiChevronDown />}
+          <FiChevronUp />
         </button>
       </div>
-      <div className="md:flex">
-        <ul
-          className={`space-y-2 md:flex flex-col ${
-            isListVisible ? "block" : "hidden"
-          }`}
-        >
+      <div className={`md:flex ${isListVisible ? "block" : "hidden"}`}>
+        <ul className="space-y-2 md:flex flex-col">
           {popularSubreddits.map((subreddit) => (
             <li
               key={subreddit}
