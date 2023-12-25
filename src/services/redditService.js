320f1792b26ccf14f98b63435c5cbbf2c1c3dc82
Omar ♔ <o_yousaf@live.co.uk>
2023-12-25 15:01:30 +0000

    squashing bugs

--- src/services/redditService.js
+++ src/services/redditService.js
@@ -1,19 +1,13 @@
 const REDDIT_API_URL = "https://www.reddit.com";
 
 export const fetchPosts = async (subreddit, searchTerm) => {
-  const url = `${REDDIT_API_URL}/r/${subreddit}${searchTerm ? `/search.json?q=${searchTerm}` : ''}`;
+  let url = `${REDDIT_API_URL}/r/${subreddit}.json`;
 
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    if (!data || !data.data || !data.data.children) {
-      throw new Error("Invalid API response");
-    }
-
-    return data.data.children.map((post) => post.data);
-  } catch (error) {
-    console.error("Error fetching posts:", error);
-    return [];
+  if (searchTerm) {
+    url = `${REDDIT_API_URL}/r/${subreddit}/search.json?q=${searchTerm}`;
   }
+
+  const response = await fetch(url);
+  const data = await response.json();
+  return data.data.children.map((post) => post.data);
 };
