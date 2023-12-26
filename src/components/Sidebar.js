c6ebe47c324ce01740e56866f9d386a243cfa443
Omar ♔ <o_yousaf@live.co.uk>
2023-12-26 19:57:14 +0000

    squashing bugs

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -47,24 +47,18 @@ const Sidebar = () => {
           <FiChevronDown />
         </button>
       </div>
-      <div
-        className={`md:flex justify-center space-y-2 transition-all duration-300 ${
-          isOpen ? "max-h-screen" : "max-h-0"
-        } `}
-      >
-        {isOpen && (
-          <ul className="md:flex flex-col space-y-2 overflow-hidden">
-            {popularSubreddits.map((subreddit) => (
-              <li
-                key={subreddit}
-                className="text-white hover:text-gray-300 cursor-pointer"
-                onClick={() => handleSubredditClick(subreddit)}
-              >
-                r/{subreddit}
-              </li>
-            ))}
-          </ul>
-        )}
+      <div className={`md:flex justify-center ${isOpen ? "block" : "hidden"} max-h-screen  overflow-hidden transition-all duration-300`}>
+        <ul className="space-y-2 md:flex flex-col ">
+          {popularSubreddits.map((subreddit) => (
+            <li
+              key={subreddit}
+              className="text-white hover:text-gray-300 cursor-pointer"
+              onClick={() => handleSubredditClick(subreddit)}
+            >
+              r/{subreddit}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
